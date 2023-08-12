f2ca9d5adda3bbbbf49e5456067d85de216150fe
Laraib Ahmad <112575218+Laraib17@users.noreply.github.com>
2023-08-12 20:09:23 +0530

    Update arr_vec2.cpp

--- c++/course/arr_vec2.cpp
+++ c++/course/arr_vec2.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
+//including vector
 #include <vector>
 using namespace std;
 // Using Loops in vector
 int main()
 {
     vector<int> v;
+    //taking input
     for (int i = 0; i < 5; i++)
     {
         int elements;
         cin >> elements;
         v.push_back(elements);
     }
+    //output
     for (int i = 0; i < v.size(); i++)
     {
         cout << v[i] << " ";
     }
     cout << endl;
+    //another way
     for (int ele:v)
     {
         cout<<ele<<"   ";
